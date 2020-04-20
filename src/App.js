3b05d11f083478d5f614a72cd651267f94c923e5
rhodyapps <rhodyapps@gmail.com>
2020-04-20 07:32:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+// import PostList from './posts/PostsList'
+// import StudentList from './students/StudentList'
+import RecipesList from './recipes/RecipesList'
+
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <RecipesList />
+      </div>
+      
+    )
+  }
+}
+
+export default App
