15b10002a2922b3827f226c7d4c07f7de037a3c4
tnewman <tnewman@meditech.com>
2020-10-13 10:22:31 -0400

    changes

--- src/fhir-display-data/fhir-display-data.js
+++ src/fhir-display-data/fhir-display-data.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FhirResource, fhirVersions } from "fhir-react";
 // import resource from "./resource";
 // import resource from "../data/dischargeSummary.json";
@@ -8,17 +8,16 @@ import "fhir-react/build/style.css";
 import "fhir-react/build/bootstrap-reboot.min.css";
 
 
-class FhirDisplayData 
-extends Component {
+const FhirDisplayData = () => {
 
-  render () {
+  const [data, setData] = useState(null);
 
   return (
+
     <div className="FhirDisplayData">
       <FhirResource fhirResource= {this.props.resource} FhirVersion={fhirVersions.STU3} />
     </div>
  )
-}
-}
+  }
 
-export default FhirDisplayData
+  export default FhirDisplayData
